fd6439abb923c37987494f22579392d2823b6807
p.trochymiak <piotr@trochymiak.net>
2024-01-14 21:31:57 +0100

    start

---
+++ BTCUSDT-s.js
@@ -0,0 +1,62 @@
+//
+//  skrypt wejscia na pozycje SHORT bez klikania na gieldzie ByBit
+//  jeśli staniesz się milionerem nie zapomnij kto Ci ten skrypt podarował :)  
+//  kontakt https://trochymiak.net 
+//  https://www.bybit.com/invite?ref=G8NARR 
+
+
+// ATTENTION - you use the script at your own risk, if you do not understand what is happening below, do not run it.
+
+// UWAGA - używasz tego skryptu na własną odpowiedzzialność, ie rozumiesz co się dziej niżej nie uruchamiaj go - Zachowaj Ostrożność
+
+
+const { RestClientV5 } = require('bybit-api');
+    require('dotenv').config(); // Wczytanie pliku .env z API kluczem 
+
+async function main() {
+ const client = new RestClientV5({
+        testnet: true,                   // jeśli chcemy użyć MAINET zamieniamy na -> false
+        key: process.env.TKEY,          //  tak samo tu T zamieni na M
+        secret: process.env.TSECRET,   //  tzn MSECRET  
+    });
+ try {
+       const response = await client.getOrderbook({
+            category: 'linear',
+            symbol: 'BTCUSDT',   // para
+        });
+
+ const cenaPoczatkowa = response.result.a[0][0]; // API pobiera aktualna cene
+        let cenaP = Number(cenaPoczatkowa);
+            cenaP.toFixed(2)     // ucina cyfry pozostawia tylko dwie ostatnie po kropce tzn 12.12
+        let wzrostProcentowy = 0.6;  //  ile procenty
+
+ function zwiekszCeneOWzrost(cenaP, wzrostP) {
+      let wzrost = cenaP * (wzrostP / 100 * wzrostP );  // wylicza procent 
+        wzrost = wzrost.toFixed(2);
+//      console.log('W: ' + wzrost);    // dla sprawdzenia czy wszytko dział OK
+      const cenaPoZ = cenaP - (wzrost * 2);
+      return cenaPoZ.toFixed(2);
+    }
+
+ let cenaPoZ = zwiekszCeneOWzrost(cenaP, wzrostProcentowy);  // samo się tlumaczy 
+      console.log('P: ' + cenaP);    // cena wejscia w pozycje 
+      console.log('T: ' + cenaPoZ); // TakeProfit 
+        const orderResponse = await client.submitOrder({   // dane przesylana do API 
+            category: 'linear',
+            symbol: 'BTCUSDT',
+            side: 'Sell',
+            positionIdx: 0,
+            orderType: 'Limit',
+            qty: '0.003',    // zmienna wyliczana na podstawie dziwgni + ile masz w portfelu - najlepiej ustawić ręcznie, metodą prób i błędów 
+            price: `${cenaP}`,
+            timeInForce: 'GTC',
+            takeProfit: `${cenaPoZ}`,
+            isLeverage: '0'
+        });
+        console.log(orderResponse);
+    } catch (error) {
+        console.error(error);
+  }
+}
+main();
+
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # open-order-BB
 A script that allows you to open a position using the Bybit exchange API
+
+Script for entering LONG positions without clicking on the ByBit exchange
+If you become a millionaire, don't forget who gave you this script :)
+
+## ATTENTION - you use the script at your own risk, if you do not understand what is happening below, do not run it.
+ ☠︎⚠️👾🤖☠️🙈🙉🙊🧛
+## UWAGA - używasz tego skryptu na własną odpowiedzzialność, ie rozumiesz co się dziej niżej nie uruchamiaj go - Zachowaj Ostrożność
+
+
+
+## Installation
+
+Dillinger requires [Node.js](https://nodejs.org/) v10+ to run.
+
+Install the dependencies and devDependencies and start the script.
+
+```sh
+cd open-order-BB
+npm init
+npm install bybit-api
+npm install dotenv 
+```
+
+🇵🇱
+Poleskie instrukcje instalacji potrzebych skaldników 
+
+# Instalacja Git
+https://www.atlassian.com/pl/git/tutorials/install-git
+
+Instalacja Node,js 
+https://gist.github.com/zbicin/0f1e360369b6694775dfcd52aa32fe92
+
+Instalacja NPM lub PNPM
+https://boringowl.io/tag/npm
+
+Ja używam PNPM
+https://pnpm.io/installation
+
+
+
+
+# Referal to: 
+https://www.bybit.com/invite?ref=G8NARR
+
+# Test
+https://testnet.bybit.com/en/
+
+# Bybit API Documentation
+https://bybit-exchange.github.io/docs/
+https://testnet.bybit.com/future-activity/en/developer
+https://github.com/bybit-exchange/api-usage-examples 
